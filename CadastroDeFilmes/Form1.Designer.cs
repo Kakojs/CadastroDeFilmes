91a5a0f7834474539260981d9e7a637a3d7016a6
pinka <pinka@KAUAN>
2025-02-25 20:55:41 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeFilmes/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CadastroDeFilmes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.novoFilmeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carregarDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarDadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Diretor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AnoDeLancamento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Duracao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Avaliacao = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoFilmeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // novoFilmeToolStripMenuItem
+            // 
+            this.novoFilmeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carregarDadosToolStripMenuItem,
+            this.salvarDadosToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.novoFilmeToolStripMenuItem.Name = "novoFilmeToolStripMenuItem";
+            this.novoFilmeToolStripMenuItem.Size = new System.Drawing.Size(99, 24);
+            this.novoFilmeToolStripMenuItem.Text = "Novo Filme";
+            // 
+            // carregarDadosToolStripMenuItem
+            // 
+            this.carregarDadosToolStripMenuItem.Name = "carregarDadosToolStripMenuItem";
+            this.carregarDadosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.carregarDadosToolStripMenuItem.Text = "Carregar Dados";
+            // 
+            // salvarDadosToolStripMenuItem
+            // 
+            this.salvarDadosToolStripMenuItem.Name = "salvarDadosToolStripMenuItem";
+            this.salvarDadosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salvarDadosToolStripMenuItem.Text = "Salvar Dados";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Titulo,
+            this.Diretor,
+            this.Genero,
+            this.AnoDeLancamento,
+            this.Duracao,
+            this.Avaliacao});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(788, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Titulo
+            // 
+            this.Titulo.Frozen = true;
+            this.Titulo.HeaderText = "Titulo";
+            this.Titulo.MinimumWidth = 6;
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Width = 125;
+            // 
+            // Diretor
+            // 
+            this.Diretor.Frozen = true;
+            this.Diretor.HeaderText = "Diretor";
+            this.Diretor.MinimumWidth = 6;
+            this.Diretor.Name = "Diretor";
+            this.Diretor.Width = 125;
+            // 
+            // Genero
+            // 
+            this.Genero.HeaderText = "Genero";
+            this.Genero.MinimumWidth = 6;
+            this.Genero.Name = "Genero";
+            this.Genero.Width = 125;
+            // 
+            // AnoDeLancamento
+            // 
+            this.AnoDeLancamento.HeaderText = "Ano de Lançamento ";
+            this.AnoDeLancamento.MinimumWidth = 6;
+            this.AnoDeLancamento.Name = "AnoDeLancamento";
+            this.AnoDeLancamento.Width = 125;
+            // 
+            // Duracao
+            // 
+            this.Duracao.HeaderText = "Duração (em minutos)";
+            this.Duracao.MinimumWidth = 6;
+            this.Duracao.Name = "Duracao";
+            this.Duracao.Width = 125;
+            // 
+            // Avaliacao
+            // 
+            this.Avaliacao.HeaderText = "Avaliação";
+            this.Avaliacao.MinimumWidth = 6;
+            this.Avaliacao.Name = "Avaliacao";
+            this.Avaliacao.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem novoFilmeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem carregarDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarDadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Diretor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Genero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AnoDeLancamento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Duracao;
+        private System.Windows.Forms.DataGridViewButtonColumn Avaliacao;
+    }
+}
+
---
+++ CadastroDeFilmes/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroDeFilmes
+{
+    public partial class Form1 : Form
+    {
+        private DataTable filmesTable;
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarDataTable();
+        }
+        private void InicializarDataTable()
+        {
+            filmesTable = new DataTable();
+            filmesTable.Columns.Add("Titulo", typeof(string));
+            filmesTable.Columns.Add("Diretor", typeof(string));
+            filmesTable.Columns.Add("Genero", typeof(string));
+            filmesTable.Columns.Add("Ano De Lançamento", typeof(int));
+            filmesTable.Columns.Add("Duração", typeof(int));
+            filmesTable.Columns.Add("Avaliação", typeof(int));
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
